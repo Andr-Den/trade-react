b5a6402fec71e4d3eec7e5d802c84ded7d5be18b
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-12-20 18:12:09 +0500

    add component Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Ход торгов <b>Тестовые торги на аппарат ЛОТОС</b></h1>
+    </header>
+  )
+}
+
+export default Header;
